93e325d407ea53f38d501b4039c1f41450744e0d
Zia Hawwa <rziahawwa@gmail.com>
2026-09-02 15:08:42 +0530

    snake can eat now

--- Python/snake.py
+++ Python/snake.py
@@ -15,11 +15,15 @@ def check_coords(new_y, new_x):
 def draw_border(stdscr):
         win = curses.newwin(20, 100, 2, 4)
         win.border(0, 0, 0, 0, 0, 0, 0, 0) 
+        curses.start_color()
+        curses.curs_set(0)
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
         cood = [(9, 49)]
         food = (food_y, food_x) = (random.randint(1, 18), random.randint(1, 98))
         while True:
-            win.addch(cood[0][0], cood[0][1], '■')
-            win.addch(food_y, food_x, '●')
+            win.addch(cood[0][0], cood[0][1], '■', curses.color_pair(1))
+            win.addch(food_y, food_x, '●', curses.color_pair(2))
             win.refresh()
             xy = win.getch()
 
@@ -37,7 +41,13 @@ def draw_border(stdscr):
                 new_head = (cood[0][0], cood[0][1] + 1)
 
             if new_head is not None and check_coords(new_head[0], new_head[1]):
-                win.addch(cood[0][0], cood[0][1], ' ')
-                cood = [new_head]
+                if new_head == food:
+                    cood.insert(0, new_head)
+                    food = (food_y, food_x) = (random.randint(1, 18), random.randint(1, 98))
+                else:
+                    tail = cood[-1]
+                    cood.insert(0, new_head)
+                    cood.pop()
+                    win.addch(tail[0], tail[1], ' ', curses.color_pair(0))
 
-curses.wrapper(draw_border)
+curses.wrapper(draw_border) 
